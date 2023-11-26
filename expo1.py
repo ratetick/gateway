9dc46696ecb948e40271b715a74d0726041ae583
vladdubinin <vladimirdubinin@yahoo.com>
2023-11-26 19:28:29 +0000

    expo1

--- expo1.py
+++ expo1.py
@@ -5,45 +5,55 @@ import numpy as np
 import math
 
 
-def cmu(t:float):
-    return 1-math.exp(-t)
-def plot_myfun(fun):
-    xpoints=np.array(fun.lenth())
-    ypoints=np.array(fun)
-    plt.plot(xpoints, ypoints)
+def cmu(t: float):
+    return 1 - math.exp(-t)
+
+
+def plot_myfun(fun: list):
+    plt.plot(range(len(fun)), fun)
     plt.show()
 
-def find_value(i, b):
-        
 
-n = 20
-T=10
-N = 1000
-bound = [0] * (n*T)
- 
-for i in range(n*T):
-    find_value(i,bound)
-total = 0
-for j in range(N):
-    walk = 0
-    for i in range(n):
-        for j in range(m):
-            val = random.normal(loc=0, scale=1)  # 1 if random.random() > 0.5 else -1
-            walk += val / m
-        if walk >= bound[i]:
-            dist[i] += 1
-            total += 1
-            break
-xp = [0] * n
-yp = [0] * n
-for k in range(n):
-    xp[k] = k
-    yp[k] = math.log(dist[k] / total)
-    # yp[k]=dist[k]/total
-    print(f"{k}={dist[k]}")
+def find_max(i, Win, Term):
+    max = -10000
+    index = 0
+    for j in range(NPATH):
+        if Term[j] == 0:
+            if max < Win[j][i]:
+                max = Win[j][i]
+                index = j
+    Term[index] = 1
+    return max
 
-xpoints = np.array(xp)
-ypoints = np.array(yp)
 
-plt.plot(xpoints, ypoints)
-plt.show()
+def find_value(i, bound, Win, Term):
+    # print(int(math.exp(-i / n) * (1 - math.exp(-1 / n)) * NPATH))
+
+    for k in range(int(math.exp(-i / n) * (1 - math.exp(-1 / n)) * NPATH)):
+        bound[i] = find_max(i, Win, Term)
+
+
+n = 200
+T = 2
+NPATH = 100000
+INT = n * T
+bound = [0] * (INT)
+# generating Winner passes
+
+Winner = []
+Terminated = []
+for i in range(NPATH):
+    path = []
+    for j in range(INT):
+        if j > 0:
+            path.append(path[j - 1] + random.normal(loc=0, scale=1))
+        else:
+            path.append(random.normal(loc=0, scale=1))
+    Winner.append(path)
+    Terminated.append(0)
+
+for i in range(INT):
+    find_value(i, bound, Winner, Terminated)
+
+plot_myfun(bound)
+print(bound)
